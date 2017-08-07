fd57a3848db99f752cb5142d622f6b11e11a6f79
Shimberly <kimmy_11_07@hotmail.com>
2017-08-07 07:30:34 +0200

    arreglos crear cuento

--- js/main.js
+++ js/main.js
@@ -4,10 +4,11 @@ var usuarios=[];
 var cuentos=[];
 
 function Usuario(user,pass,name){
-  this.user= user,
-  this.pass=pass,
-  this.name=name,
-   idUsuario++;  
+    this.user= user,
+    this.pass=pass,
+    this.name=name,
+    idUsuario++;
+    cuentos=[];
 };
 
 function Cuento(nombre){
@@ -27,35 +28,48 @@ usuarios.push=usuario1;
 usuarios.push=usuario2;
 usuarios.push=usuario3;
 
-var cuento1= new Usuario('Caperucita Roja');
-var cuento2= new Usuario('Los 3 chanchitos');
-var cuento3= new Usuario('Blancanieves');
+var cuento1= new Cuento('Caperucita Roja');
+var cuento2= new Cuento('Los 3 chanchitos');
+var cuento3= new Cuento('Blancanieves');
+cuentos.push=cuento1;
+cuentos.push=cuento2;
+cuentos.push=cuento3;
 
 var contCuento=6;
-var texto="<div> \
-            <input type='radio' name='radio-btn' id='img-" + contCuento + "' checked />\
-            <li class='slide-container'>\
-                <div class='slide'>\
-                <div class='miniTexto'>Pagina "+contCuento+"</div>\
-                </div>\
-                <div class='nav'>\
-                    <label for='img-" + (contCuento-1 )+ "' class='prev'>&#x2039;</label>\
-                    <label for='img-1' class='next'>&#x203a;</label>\
-                </div>\
-            </li>\
-            </div>"
+
 
             
 $(document).ready(function() {
     
+    /*Agregar otra hoja*/
     $(".nHoja").click(function(){
-                              
-        $(".slides").append(texto);
-        $(".nav-dots").append("<label for='img-"+contCuento+"' class='nav-dot' id='img-dot-"+contCuento+"'><div class='hojas'>"+contCuento+"</div></label>");
+        var texto="<div class='item'>\
+                      <div class='container'>\
+                        Pagina "+contCuento+"\
+                      </div>\
+                    </div>"
+                    
+        $(".carousel-inner").append(texto);
+        $(".nav-dots").append("<li data-target='#myCarousel' data-slide-to="+contCuento+" class='nav-dot'><div class='hojas'>"+contCuento+"</div></li>");
         contCuento++;
-        $(".prevInicial").addC
     });
     
     
+    /*Guardar Cuento*/
+     $("#guardar").click(function(){
+        var cuento= new Cuento($("#nombre").val());
+        cuentos.push=cuento;
+        alert("Se guardo el cuento "+cuento.nombre);
+    });
+    
+    /*Salir pero con alerta*/
+     $("#salir").click(function(){
+        window.onbeforeunload = confirmExit;
+        function confirmExit()
+        {
+            return "Seguro desea salir?";
+        }
+    });
+   
     
 });
