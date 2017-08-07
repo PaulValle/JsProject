f426ddc1fa59a95a805f193ec13f8a2dc96bb6d4
Shimberly <kimmy_11_07@hotmail.com>
2017-08-07 07:11:20 +0200

    arreglos crear cuento

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var idUsuario=0;
 var idCuento=0;
 var usuarios=[];
-var cuentos[];
+var cuentos=[];
 
 function Usuario(user,pass,name){
   this.user= user,
@@ -13,8 +13,8 @@ function Usuario(user,pass,name){
 function Cuento(nombre){
     idCuento++;  
     this.nombre=nombre;
-    var imagenes[];
-    var sonidos[];
+    var imagenes=[];
+    var sonidos=[];
     
     
 };
@@ -31,9 +31,31 @@ var cuento1= new Usuario('Caperucita Roja');
 var cuento2= new Usuario('Los 3 chanchitos');
 var cuento3= new Usuario('Blancanieves');
 
+var contCuento=6;
+var texto="<div> \
+            <input type='radio' name='radio-btn' id='img-" + contCuento + "' checked />\
+            <li class='slide-container'>\
+                <div class='slide'>\
+                <div class='miniTexto'>Pagina "+contCuento+"</div>\
+                </div>\
+                <div class='nav'>\
+                    <label for='img-" + (contCuento-1 )+ "' class='prev'>&#x2039;</label>\
+                    <label for='img-1' class='next'>&#x203a;</label>\
+                </div>\
+            </li>\
+            </div>"
 
-
-
-
+            
 $(document).ready(function() {
+    
+    $(".nHoja").click(function(){
+                              
+        $(".slides").append(texto);
+        $(".nav-dots").append("<label for='img-"+contCuento+"' class='nav-dot' id='img-dot-"+contCuento+"'><div class='hojas'>"+contCuento+"</div></label>");
+        contCuento++;
+        $(".prevInicial").addC
+    });
+    
+    
+    
 });
