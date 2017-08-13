57b5527ba51f69962ea65a1aa0e70ac1579bae24
GiPaolaOrtiz <paogi_ortiz@hotmail.com>
2017-08-13 01:59:02 -0500

    combo crear preguntas

--- js/main.js
+++ js/main.js
@@ -1,337 +1,351 @@
+class Usuario {
 
+    constructor(obj) {
+        this.user = obj.user;
+        this.pass = obj.pass;
+        this.nombre = obj.nombre;
+        var cuentos = [];
+        $.each(obj.cuentos, function (i, emp) {
 
-
-class Usuario{
-    
-  constructor(obj) {
-        this.user= obj.user;
-        this.pass=obj.pass;
-        this.nombre=obj.nombre;
-        var cuentos=[];
-        $.each(obj.cuentos,function(i,emp){ 
-            
-            var cnt=new Cuento();
+            var cnt = new Cuento();
             cnt.conObj(emp)
             cuentos.push(cnt);
         });
-        this.cuentos=cuentos;
-  }
+        this.cuentos = cuentos;
+    }
 };
 
-class Cuento{
-  
-   
-    conObj(obj){
+class Cuento {
+
+
+    conObj(obj) {
         //alert(obj.nombre);
-        this.nombre=obj.nombre;
-        var imagenes=[];
-        $.each(obj.imagenes,function(i,emp){ 
+        this.nombre = obj.nombre;
+        var imagenes = [];
+        $.each(obj.imagenes, function (i, emp) {
             imagenes.push(emp.url);
         });
-        this.imagenes=imagenes;
-        var audios=[];
-        $.each(obj.audios,function(i,emp){ 
+        this.imagenes = imagenes;
+        var audios = [];
+        $.each(obj.audios, function (i, emp) {
             audios.push(emp.url);
         });
-        this.audios=audios;
+        this.audios = audios;
     }
     directo(nombre) {
-        this.nombre=nombre;
-        var imagenes=[];
-        this.imagenes=imagenes;
-        var audios=[];
-        this.audios=audios;
+        this.nombre = nombre;
+        var imagenes = [];
+        this.imagenes = imagenes;
+        var audios = [];
+        this.audios = audios;
     }
 };
 
 
-var contCuento=6;
+var contCuento = 6;
 
-    
 
-    function leer() {
-        var usuarios=[];
-	    
-        $.getJSON('datos.json',function(data){
-           
-            $.each(data.usuarios,function(i,emp){
-                //alert("hola");
-                //var usr= new Usuario();
-                usuarios.push(new Usuario(emp));
-            });
-            /*
-            $.each(usuarios,function(i,emp){ 
-                alert(emp.nombre);
-            });*/
-           
+
+function leer() {
+    var usuarios = [];
+
+    $.getJSON('datos.json', function (data) {
+
+        $.each(data.usuarios, function (i, emp) {
+            //alert("hola");
+            //var usr= new Usuario();
+            usuarios.push(new Usuario(emp));
         });
-       return usuarios;
-        
-    };
+        /*
+        $.each(usuarios,function(i,emp){ 
+            alert(emp.nombre);
+        });*/
 
-    /*Agregar otra hoja*/
-    $(".nHoja").click(function(){
-      
-        var texto="<div class='item'>\
+    });
+    return usuarios;
+
+};
+
+/*Agregar otra hoja*/
+$(".nHoja").click(function () {
+
+    var texto = "<div class='item'>\
                       <div class='container escenas'>\
-                        <div>Pagina "+contCuento+"</div>\
+                        <div>Pagina " + contCuento + "</div>\
                       </div>\
                     </div>"
-                    
-        $(".carousel-inner").append(texto);
-        $(".nav-dots").append("<li data-target='#myCarousel' data-slide-to="+contCuento+" class='nav-dot'><div id='droppable' class='hojas'>"+contCuento+"</div></li>");
-        contCuento++;
-    });
-    /*Eliminar otra hoja*/
-        
-    $(".bHoja").click(function(){
-        contCuento--;
-        //$(".carousel-control").simulate('click');
-        
-        $(".item:last").remove();
-        $(".nav-dot:last").remove();    
+
+    $(".carousel-inner").append(texto);
+    $(".nav-dots").append("<li data-target='#myCarousel' data-slide-to=" + contCuento + " class='nav-dot'><div id='droppable' class='hojas'>" + contCuento + "</div></li>");
+    contCuento++;
+});
+/*Eliminar otra hoja*/
+
+$(".bHoja").click(function () {
+    contCuento--;
+    //$(".carousel-control").simulate('click');
+
+    $(".item:last").remove();
+    $(".nav-dot:last").remove();
+});
+
+/*Guardar Cuento*/
+$(".guard").click(function () {
+    var hola = "vales verga";
+    $.ajax({
+        url: 'guardarJson.php',
+        method: 'post',
+        data: {
+            "identificador": hola
+        },
+        success: function (data) {
+            alert(data);
+
+        }
     });
 
-    /*Guardar Cuento*/
-    $(".guard").click(function(){
-            var hola="vales verga"; 
-           $.ajax({url:'guardarJson.php',method:'post',data:{"identificador":hola},success: function(data) {
-                        alert(data);
-                       
-                    }});
-            
-    }); 
+});
 
-/*Preguntas*/
-  $(".bActividad").click(function(){
-       alert("Hola");
+/*PREGUNTAS*/
+/*Preguntas BOTON*/
+$(".bActividad").click(function () {
+    alert("Hola");
+});
+
+/*Seleccionar Combo*/
+$('select#actividad').change(function(){
+    window.location = $(this).val();
+});
+
+
+
+$("#guardar").click(function () {
+    var imagenesCuento = [];
+    //esta bandera sirve para saber si todas las hojas estan llenas
+    var flag = 0;
+    $(".escenas").each(function (index) {
+
+        var ruta = $(this).find("img").attr("src");
+        //alert("ruta: "+ruta);
+        if (ruta == undefined) {
+            alert("Llena todas las hojas.");
+            flag++;
+            return false;
+        } else {
+            imagenesCuento.push(ruta);
+
+        }
+
     });
 
-     $("#guardar").click(function(){
-        var imagenesCuento=[];
-        //esta bandera sirve para saber si todas las hojas estan llenas
-        var flag=0; 
-        $(".escenas").each(function (index) 
-        { 
-            
-            var ruta=$(this).find("img").attr("src");
-            //alert("ruta: "+ruta);
-            if(ruta==undefined){
-                alert("Llena todas las hojas.");
-                flag++;
-                return false;
-            }else{
-                imagenesCuento.push(ruta);
-                
-            }
-            
-        }); 
+    //si todas las hojas estan llenas se puede guardar sino no
+    if (flag == 0) {
+
+        /*  var usuarios=[];
+          usuarios=leer();
+          var cuento= new Cuento();
+          cuento.directo($("#nombre").val());
+          alert("Se guardo el cuento "+cuento.nombre + usuarios);
+          alert("f "+usuarios[0].cuentos.length);
+           
          
-        //si todas las hojas estan llenas se puede guardar sino no
-        if(flag==0){
-           
-          /*  var usuarios=[];
-            usuarios=leer();
-            var cuento= new Cuento();
-            cuento.directo($("#nombre").val());
-            alert("Se guardo el cuento "+cuento.nombre + usuarios);
-            alert("f "+usuarios[0].cuentos.length);
-             
-           
-            cuento.imagenes=imagenesCuento;
-             //alert(imagenesCuento.length);
-            usuarios[0].cuentos.push(cuento); 
-            alert("t"+usuarios[0].cuentos.length);
-            let dataStr = JSON.stringify(usuarios);
-            let dataUri = 'data:application/json;charset=utf-8,' + encodeURIComponent(dataStr);
+          cuento.imagenes=imagenesCuento;
+           //alert(imagenesCuento.length);
+          usuarios[0].cuentos.push(cuento); 
+          alert("t"+usuarios[0].cuentos.length);
+          let dataStr = JSON.stringify(usuarios);
+          let dataUri = 'data:application/json;charset=utf-8,' + encodeURIComponent(dataStr);
 
-            //let exportFileDefaultName = 'data.json';
-            //let linkElement = $(".botonesHojas").append("<a href='"+dataUri+"' download='"+exportFileDefaultName+"'>Bajar</a>");*/
-            
-            
-            
-           // window.location.href = "../index.html";
-            
-        }
-        
-    });  
+          //let exportFileDefaultName = 'data.json';
+          //let linkElement = $(".botonesHojas").append("<a href='"+dataUri+"' download='"+exportFileDefaultName+"'>Bajar</a>");*/
 
-    /*Salir pero con alerta*/
-     $("#salir").click(function(){
-        window.onbeforeunload = confirmExit;
-        function confirmExit()
-        {
-            return "Seguro desea salir?";
-        }
-    });
-    
-    
-    //SUBIR ARCHIVOS AUDIO Y SONIDO
-    
-    $(".messages").hide();
-    //queremos que esta variable sea global
-    var fileExtension = "";
-    //función que observa los cambios del campo file y obtiene información
-   
-    $('#imagen').change(function()
-    {
-        //obtenemos un array con los datos del archivo
-        var file = $("#imagen")[0].files[0];
-        //obtenemos el nombre del archivo
-        var fileName = file.name;
-        //obtenemos la extensión del archivo
-        fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
-        //obtenemos el tamaño del archivo
-        var fileSize = file.size;
-        //obtenemos el tipo de archivo image/png ejemplo
-        var fileType = file.type;
-        //mensaje con la información del archivo
-        showMessageE("<span>Archivo para subir: "+fileName+", peso total: "+fileSize+" bytes.</span>");
-    });
 
-    $('#audio').change(function()
-    {
-        //obtenemos un array con los datos del archivo
-        var file = $("#audio")[0].files[0];
-        //obtenemos el nombre del archivo
-        var fileName = file.name;
-        //obtenemos la extensión del archivo
-        fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
-        //obtenemos el tamaño del archivo
-        var fileSize = file.size;
-        //obtenemos el tipo de archivo image/png ejemplo
-        var fileType = file.type;
-        //mensaje con la información del archivo
-        showMessageA("<span>Archivo para subir: "+fileName+", peso total: "+fileSize+" bytes.</span>");
-    });
 
-    //al enviar el formulario
-    $('.subirImg').click(function(){
-        //información del formulario
-        var formData = new FormData($(".formularioI")[0]);
-        var message = ""; 
-        //hacemos la petición ajax  
-        $.ajax({
-            url: 'subir.php',  
-            type: 'POST',
-            // Form data
-            //datos del formulario
-            data: formData,
-            //necesario para subir archivos via ajax
-            cache: false,
-            contentType: false,
-            processData: false,
-            //mientras enviamos el archivo
-            beforeSend: function(){
-                message = $("<span>Subiendo la imagen, por favor espere...</span>");
-                showMessageE(message)        
-            },
-            //una vez finalizado correctamente
-            success: function(data){
-                message = $("<span>La imagen ha subido correctamente.</span>");
-                showMessageE(message);
-                if(isImage(fileExtension))
-                {
-                    $(".fondoEscenas").html("<img id='draggable' class='ui-widget-content' src='../img/cuentos/"+data+"' />");
-                     $( "#draggable" ).draggable({ revert: true});
-                }
-            },
-            //si ha ocurrido un error
-            error: function(){
-                message = $("<span>Ha ocurrido un error.</span>");
-                showMessageE(message);
+        // window.location.href = "../index.html";
+
+    }
+
+});
+
+/*Salir pero con alerta*/
+$("#salir").click(function () {
+    window.onbeforeunload = confirmExit;
+
+    function confirmExit() {
+        return "Seguro desea salir?";
+    }
+});
+
+
+//SUBIR ARCHIVOS AUDIO Y SONIDO
+
+$(".messages").hide();
+//queremos que esta variable sea global
+var fileExtension = "";
+//función que observa los cambios del campo file y obtiene información
+
+$('#imagen').change(function () {
+    //obtenemos un array con los datos del archivo
+    var file = $("#imagen")[0].files[0];
+    //obtenemos el nombre del archivo
+    var fileName = file.name;
+    //obtenemos la extensión del archivo
+    fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
+    //obtenemos el tamaño del archivo
+    var fileSize = file.size;
+    //obtenemos el tipo de archivo image/png ejemplo
+    var fileType = file.type;
+    //mensaje con la información del archivo
+    showMessageE("<span>Archivo para subir: " + fileName + ", peso total: " + fileSize + " bytes.</span>");
+});
+
+$('#audio').change(function () {
+    //obtenemos un array con los datos del archivo
+    var file = $("#audio")[0].files[0];
+    //obtenemos el nombre del archivo
+    var fileName = file.name;
+    //obtenemos la extensión del archivo
+    fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
+    //obtenemos el tamaño del archivo
+    var fileSize = file.size;
+    //obtenemos el tipo de archivo image/png ejemplo
+    var fileType = file.type;
+    //mensaje con la información del archivo
+    showMessageA("<span>Archivo para subir: " + fileName + ", peso total: " + fileSize + " bytes.</span>");
+});
+
+//al enviar el formulario
+$('.subirImg').click(function () {
+    //información del formulario
+    var formData = new FormData($(".formularioI")[0]);
+    var message = "";
+    //hacemos la petición ajax  
+    $.ajax({
+        url: 'subir.php',
+        type: 'POST',
+        // Form data
+        //datos del formulario
+        data: formData,
+        //necesario para subir archivos via ajax
+        cache: false,
+        contentType: false,
+        processData: false,
+        //mientras enviamos el archivo
+        beforeSend: function () {
+            message = $("<span>Subiendo la imagen, por favor espere...</span>");
+            showMessageE(message)
+        },
+        //una vez finalizado correctamente
+        success: function (data) {
+            message = $("<span>La imagen ha subido correctamente.</span>");
+            showMessageE(message);
+            if (isImage(fileExtension)) {
+                $(".fondoEscenas").html("<img id='draggable' class='ui-widget-content' src='../img/cuentos/" + data + "' />");
+                $("#draggable").draggable({
+                    revert: true
+                });
             }
-        });
+        },
+        //si ha ocurrido un error
+        error: function () {
+            message = $("<span>Ha ocurrido un error.</span>");
+            showMessageE(message);
+        }
     });
+});
 
 
 //AUDIOS
 
-$('.subirAudio').click(function(){
-        //información del formulario
-        var formData = new FormData($(".formularioA")[0]);
-        var message = ""; 
-        //hacemos la petición ajax  
-        $.ajax({
-            url: 'subir.php',  
-            type: 'POST',
-            // Form data
-            //datos del formulario
-            data: formData,
-            //necesario para subir archivos via ajax
-            cache: false,
-            contentType: false,
-            processData: false,
-            //mientras enviamos el archivo
-            beforeSend: function(){
-                message = $("<span\>Subiendo el audio, por favor espere...</span>");
-                showMessageA(message)        
-            },
-            //una vez finalizado correctamente
-            success: function(data){
-                message = $("<span\>El audio ha subido correctamente.</span>");
-                showMessageA(message);
-                if(isImage(fileExtension))
-                {
-                    $(".fondoAudio").append("<audio controls><source src='../img/cuentos/" +data+ "' type='audio/mp3'></audio>");
-                    console.log(data);
-                 /*<audio controls>
+$('.subirAudio').click(function () {
+    //información del formulario
+    var formData = new FormData($(".formularioA")[0]);
+    var message = "";
+    //hacemos la petición ajax  
+    $.ajax({
+        url: 'subir.php',
+        type: 'POST',
+        // Form data
+        //datos del formulario
+        data: formData,
+        //necesario para subir archivos via ajax
+        cache: false,
+        contentType: false,
+        processData: false,
+        //mientras enviamos el archivo
+        beforeSend: function () {
+            message = $("<span\>Subiendo el audio, por favor espere...</span>");
+            showMessageA(message)
+        },
+        //una vez finalizado correctamente
+        success: function (data) {
+            message = $("<span\>El audio ha subido correctamente.</span>");
+            showMessageA(message);
+            if (isImage(fileExtension)) {
+                $(".fondoAudio").append("<audio controls><source src='../img/cuentos/" + data + "' type='audio/mp3'></audio>");
+                console.log(data);
+                /*<audio controls>
                               <source src="../img/cuentos/000938162_prev.mp3" type="audio/mp3">
                 </audio>*/
-                }
-            },
-            //si ha ocurrido un error
-            error: function(){
-                message = $("<span>Ha ocurrido un error.</span>");
-                showMessageA(message);
             }
-        });
+        },
+        //si ha ocurrido un error
+        error: function () {
+            message = $("<span>Ha ocurrido un error.</span>");
+            showMessageA(message);
+        }
     });
-    
+});
+
 
 
 
 //como la utilizamos demasiadas veces, creamos una función para 
 //evitar repetición de código
-function showMessageE(message){
+function showMessageE(message) {
     $(".messagesE").html("").show();
     $(".messagesE").html(message);
 }
-function showMessageA(message){
+
+function showMessageA(message) {
     $(".messagesA").html("").show();
     $(".messagesA").html(message);
 }
 //comprobamos si el archivo a subir es una imagen
 //para visualizarla una vez haya subido
-function isImage(extension)
-{
-    switch(extension.toLowerCase()) 
-    {
-        case 'jpg': case 'gif': case 'png': case 'jpeg':
+function isImage(extension) {
+    switch (extension.toLowerCase()) {
+        case 'jpg':
+        case 'gif':
+        case 'png':
+        case 'jpeg':
             return true;
-        break;
-        case 'mp3': case 'wav': case 'mp4': case 'ogg':
+            break;
+        case 'mp3':
+        case 'wav':
+        case 'mp4':
+        case 'ogg':
             return true;
-        break;
+            break;
         default:
             return false;
-        break;
+            break;
     }
-      }
-    
-    /// //FIN ARCHIVOS AUDIO Y SONIDO
-  
+}
+
+/// //FIN ARCHIVOS AUDIO Y SONIDO
+
 /*ARRASTRAR IMAGENES*/
 
- $( function() {
-    $( ".item" ).droppable({
-      drop: function( event, ui ) {
-         
-          var id = ui.draggable.attr("src");
-          //alert(id);
-          //alert(this);
-        
-        $( this ).children().html("<img src='"+id+"'>");
-      }
+$(function () {
+    $(".item").droppable({
+        drop: function (event, ui) {
+
+            var id = ui.draggable.attr("src");
+            //alert(id);
+            //alert(this);
+
+            $(this).children().html("<img src='" + id + "'>");
+        }
     });
-  } );
+});
